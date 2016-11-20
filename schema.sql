ed0f8c027e26c5895addf30afafa150edb1490ef
Chris Stoafer <chris.stoafer@capitalone.com>
2016-11-20 18:38:10 -0500

    remove sqlite schema

--- schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists representatives;
-create table representatives (
-    id integer primary key autoincrement,
-    first_name text not null,
-    last_name text not null,
-    state text not null,
-    district text not null,
-    phone text not null
-);

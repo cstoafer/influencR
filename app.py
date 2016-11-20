f7ce6cd700ef04fdccf495a87483d1bf96d8c734
Chris Stoafer <chris.stoafer@capitalone.com>
2016-11-20 18:10:49 -0500

    removed all sqlite calls

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import os
 
-#from influencR import create_app, db
-import sqlite3
 from flask import (Flask, request, session, g, redirect, url_for,
                    abort, render_template, flash)
 from flask_sqlalchemy import SQLAlchemy
-#from flask.ext.sqlalchemy import SQLAlchemy
 from contextlib import closing
 
 
@@ -16,47 +13,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import Representative
-#app = create_app()
 
-'''
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-'''
 
 def get_state_district_by_zipcode(zipcode):
     return ('NY','1')
 
+
 def get_representative_from_zipcode(zipcode):
-    state, district = get_state_district_by_zipcode(zipcode)
-    query = ('SELECT first_name, last_name, phone '
-             'FROM representatives '
-             'WHERE state = "{}" AND district = "{}";'.format(state,district))
-    cur = g.db.execute(query)
-    rep_rows = cur.fetchall()
-    if len(rep_rows) == 0:
-        raise ValueError('representative not found for zipcode')
-    assert(len(rep_rows) == 1)
-    rep_row = rep_rows[0]
-    rep = dict(fname=rep_row[0], lname=rep_row[1], phone=rep_row[2])
-    return rep
+    return {'fname': 'Cheatin\'', 'lname': 'Charlie',
+            'phone': 'This isn\'t even a phone number'}
 
-'''
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
-'''
 
 @app.route('/', methods=['GET', 'POST'])
 def show_homepage():
@@ -65,6 +31,7 @@ def show_homepage():
         rep = get_representative_from_zipcode(zipcode)
         return render_template('contactinfo.html', rep=rep)
     return render_template('home.html')
+
 
 @app.route('/contactinfo')
 def show_contact_info():

8450a55700ef5db6c87e150ffc8764363186ec87
Chris Stoafer <chris.stoafer@capitalone.com>
2016-11-20 12:16:26 -0500

    test simple app.py on heroku

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import (Flask, request, session, g, redirect, url_for,
 from contextlib import closing
 
 # configuration
-DATABASE = 'data/db.db'
+#DATABASE = 'data/db.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 
@@ -14,6 +14,7 @@ SECRET_KEY = 'development key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+'''
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
@@ -22,6 +23,7 @@ def init_db():
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+'''
 
 def get_state_district_by_zipcode(zipcode):
     return ('NY','1')
@@ -40,6 +42,7 @@ def get_representative_from_zipcode(zipcode):
     rep = dict(fname=rep_row[0], lname=rep_row[1], phone=rep_row[2])
     return rep
 
+'''
 @app.before_request
 def before_request():
     g.db = connect_db()
@@ -49,6 +52,7 @@ def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
+'''
 
 @app.route('/', methods=['GET', 'POST'])
 def show_homepage():

bc5bbf0bc2bef59a171d31244cf7809ba433501f
Chris Stoafer <chris.stoafer@capitalone.com>
2016-11-20 18:12:01 -0500

    remove unneccessary import

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from flask import (Flask, request, session, g, redirect, url_for,
                    abort, render_template, flash)
 from flask_sqlalchemy import SQLAlchemy
-from contextlib import closing
 
 
 # create the application
